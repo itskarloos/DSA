917c804c23942cd4f9798f6152b8bce8dac6ff37
Michael <mikenegh@gmail.com>
2024-12-27 16:43:19 +0300

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
-
+    const size = arr.length;
+    let temp;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; i - j >= 0; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
