08c1a2751ce2d1569fd59ba9eda908f44d725be4
Michael <mikenegh@gmail.com>
2024-12-27 15:33:21 +0300

    two crystall ball solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const floors = breaks.length;
+    const steps = Math.floor(Math.sqrt(floors));
+    for (let i = 0; i < floors; i += steps) {
+        if (breaks[i]) {
+            for (let j = 0; j < steps && i - j >= 0; j++) {
+                {
+                    if (breaks[i - j] === false) {
+                        return i - j + 1;
+                    }
+                }
+            }
+        }
+    }
+    return -1;
 }
