03fd8b1546a22737ac8fb6ce128bb00a25f0a714
Michael <mikenegh@gmail.com>
2024-12-27 17:04:43 +0300

    Bubble sort fix

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -2,7 +2,7 @@ export default function bubble_sort(arr: number[]): void {
     const size = arr.length;
     let temp;
     for (let i = 0; i < size; i++) {
-        for (let j = 0; i - j >= 0; j++) {
+        for (let j = 0; size - i > j; j++) {
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
