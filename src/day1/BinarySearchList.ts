8a1352b630161da8bcd06b63f4ef1b2d3786501d
Michael <mikenegh@gmail.com>
2024-12-27 14:38:48 +0300

    Binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    let mid = Math.floor(low + (high - low) / 2);
+    do {
+        if (haystack[mid] === needle) {
+            console.log(mid);
+            return true;
+        } else if (haystack[mid] > needle) {
+            high = mid;
+            mid = Math.floor(low + (high - low) / 2);
+        } else if (haystack[mid] < needle) {
+            low = mid + 1;
+            mid = Math.floor(low + (high - low) / 2);
+        }
+    } while (low < high);
+    return false;
 }
