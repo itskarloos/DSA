d0153941a9b65d62a787d469a9f9c58da4e94b30
Michael <mikenegh@gmail.com>
2025-02-03 14:18:00 +0300

    Linked list queue with my own implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -8,11 +8,33 @@ export default class Queue<T> {
     public tail?: Node<T>;
 
     constructor() {
-        this.head = this.tail = undefined;
+        this.tail = this.head = undefined;
         this.length = 0;
     }
 
-    enqueue(item: T): void {}
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+    enqueue(item: T): void {
+        if (this.length === 0) {
+            const node = { value: item } as Node<T>;
+            this.length++;
+            this.head = this.tail = node;
+            return;
+        }
+        const node = { value: item } as Node<T>;
+        this.tail?.next = node;
+        this.tail = node;
+        this.length++;
+    }
+    deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+        head.next = undefined;
+        return head.value;
+    }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
