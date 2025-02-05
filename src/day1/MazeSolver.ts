8fb7792d68cf5454b8641fc3b40847aa00ec5d8e
Michael <mikenegh@gmail.com>
2025-02-05 12:33:36 +0300

    Maze path finder base case

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,17 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
-
+function walk(maze: string[], curr: Point, end: Point, wall: Point): boolean {
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
+    return true;
 }
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}
