484d0bccc6808119f1d13b144722b18709f1cccb
Michael <mikenegh@gmail.com>
2025-02-05 12:38:41 +0300

    Path added

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,10 +1,27 @@
-function walk(maze: string[], curr: Point, end: Point, wall: Point): boolean {
+function walk(
+    maze: string[],
+    curr: Point,
+    end: Point,
+    wall: Point,
+    seen: boolean[][],
+    path: Point[],
+): boolean {
     if (
         curr.x < 0 ||
         curr.x >= maze[0].length ||
         curr.y < 0 ||
         curr.y >= maze.length
     ) {
+        return false;
+    }
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    if (curr.x === end.x || curr.y === end.y) {
+        return true;
+    }
+    if (seen[curr.x][curr.y]) {
         return false;
     }
     return true;
