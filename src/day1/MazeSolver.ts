5570281cc8d6cd38605a25307606d99cade22c12
Michael <mikenegh@gmail.com>
2025-02-05 13:53:06 +0300

    Maze world problem solution

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,8 +1,14 @@
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
 function walk(
     maze: string[],
+    wall: string,
     curr: Point,
     end: Point,
-    wall: Point,
     seen: boolean[][],
     path: Point[],
 ): boolean {
@@ -18,17 +24,39 @@ function walk(
         return false;
     }
 
-    if (curr.x === end.x || curr.y === end.y) {
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(curr);
         return true;
     }
-    if (seen[curr.x][curr.y]) {
+    if (seen[curr.y][curr.x]) {
         return false;
     }
-    return true;
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+    for (let i = 0; i < dir.length; ++i) {
+        const [x, y] = dir[i];
+        if (
+            walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path)
+        ) {
+            return true;
+        }
+    }
+    path.pop();
+    return false;
 }
 export default function solve(
     maze: string[],
     wall: string,
     start: Point,
     end: Point,
-): Point[] {}
+): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze.length; ++i) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, end, start, seen, path);
+    return path;
+}
