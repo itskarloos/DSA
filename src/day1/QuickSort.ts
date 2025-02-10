e1462e5fcd5edc64c8f11619227eeafcf1690b8b
Michael <mikenegh@gmail.com>
2025-02-10 16:34:10 +0300

    Initial commit

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,14 @@
+function partition(numbers: number[], lo: number, hi: number): number {
+  let pivot = numbers[hi];
+  let idx = lo - 1;
+
+  for (let i = 0; i < numbers[hi]; ++i) {
+    if(pivot< numbers[i])
+  }
+
+
+
+}
 export default function quick_sort(arr: number[]): void {
 
 }
