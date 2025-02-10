172def551f1a0a8111b48a0d30b60308c5c589a7
Michael <mikenegh@gmail.com>
2025-02-10 16:51:16 +0300

    quick sort implementation

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,14 +1,26 @@
-function partition(numbers: number[], lo: number, hi: number): number {
-  let pivot = numbers[hi];
-  let idx = lo - 1;
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
+    const pivotIdx = partition(arr, lo, hi);
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
+}
+function partition(arr: number[], lo: number, hi: number): number {
+    let pivot = arr[hi];
+    let idx = lo - 1;
 
-  for (let i = 0; i < numbers[hi]; ++i) {
-    if(pivot< numbers[i])
-  }
+    for (let i = lo; i < arr[hi]; ++i) {
+        if (arr[i] <= pivot) {
+            idx++;
+            let temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+    }
 
-
-
+    return idx;
 }
 export default function quick_sort(arr: number[]): void {
-
+    qs(arr, 0, arr.length - 1);
 }
