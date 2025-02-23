ad16070e4aa8d5a7e59267182235a24f592d41ba
Michael <mikenegh@gmail.com>
2025-02-23 17:18:52 +0300

    dfs study

---
+++ src/day1/dfs.py
@@ -0,0 +1,20 @@
+visited = set()
+graph = {
+    'A': ['B', 'D'],
+    'B': ['A', 'C', 'E'],
+    'C': ['B'],
+    'D': ['A', 'E'],
+    'E': ['B', 'D']
+}
+
+
+def dfs(node):
+    if node in visited:
+        return
+    visited.add(node)
+    print(node)
+    for neighbour in graph[node]:
+        return dfs(neighbour)
+
+
+dfs("D")
